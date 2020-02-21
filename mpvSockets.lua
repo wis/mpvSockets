8c8e0482727621159925ccae6bd64e32daa4e28d
frh <4987254+frh@users.noreply.github.com>
2020-02-21 12:54:29 +0100

    Use mp.utils.getpid()

--- mpvSockets.lua
+++ mpvSockets.lua
@@ -3,17 +3,6 @@
 local utils = require 'mp.utils'
 tempDir = "/tmp"
 
-function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  s = string.gsub(s, '[\n\r]+', ' ')
-  return s
-end
-
 function join_paths(...)
     local arg={...}
     path = ""
@@ -23,7 +12,7 @@ function join_paths(...)
     return path;
 end
 
-ppid = os.capture("stat=($(</proc/$$/stat)) && echo \"${stat[3]}\"", false)
+ppid = utils.getpid()
 os.execute("mkdir " .. join_paths(tempDir, "mpvSockets") .. " 2>/dev/null")
 mp.set_property("options/input-ipc-server", join_paths(tempDir, "mpvSockets", ppid))
 
